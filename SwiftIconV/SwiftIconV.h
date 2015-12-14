4e93e23c3052c4a60fb298faa53e7cc581ddcc4f
C.W. Betts <computers57@hotmail.com>
2015-12-14 01:55:18 -0700

    Initial Commit

---
+++ SwiftIconV/SwiftIconV.h
@@ -0,0 +1,19 @@
+//
+//  SwiftIconV.h
+//  SwiftIconV
+//
+//  Created by C.W. Betts on 12/14/15.
+//  Copyright © 2015 C.W. Betts. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftIconV.
+FOUNDATION_EXPORT double SwiftIconVVersionNumber;
+
+//! Project version string for SwiftIconV.
+FOUNDATION_EXPORT const unsigned char SwiftIconVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftIconV/PublicHeader.h>
+
+
---
+++ SwiftIconVTests/SwiftIconVTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftIconVTests.swift
+//  SwiftIconVTests
+//
+//  Created by C.W. Betts on 12/14/15.
+//  Copyright © 2015 C.W. Betts. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftIconV
+
+class SwiftIconVTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
